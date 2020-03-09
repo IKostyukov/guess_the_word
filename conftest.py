766ac694a4f32a9e29c0f43b17c21edef7140a13
IKostyukov <52851162+IKostyukov@users.noreply.github.com>
2020-03-09 16:07:18 +0300

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,32 @@
+import pytest
+import app
+
+
+app.lit = "i"
+
+
+@pytest.fixture
+def big_count():
+    app.count = 3
+ 
+
+@pytest.fixture
+def ok_1():
+    app.word = "pip"
+
+@pytest.fixture
+def ok_3():
+    app.word = "iii"
+
+# @pytest.fixture
+# def lit_no_english():
+#     app.lit = "щ"
+    
+# @pytest.fixture
+# def lit_used():
+#     app.lit = "u"
+#     app.used_lit = USED_LIT
+
+# @pytest.fixture
+# def lit_ok():
+#     app.lit = lit
